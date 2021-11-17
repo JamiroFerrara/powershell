9e0bceb6ce23854fa3d76ffdd53b2725c14c84b2
Jamiro Ferrara <ferrarajamiro@hotmail.com>
2021-11-17 17:22:43 +0100

    Update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -774,8 +774,8 @@ Function Get-ESGlobalSearchResult {
     if (!(Test-Path (Resolve-Path $esPath).Path)){
         Write-Warning "Everything commandline es.exe could not be found on the system please download and install via http://www.voidtools.com/es.zip"
     }
-    $result = & (Resolve-Path $esPath).Path $SearchTerm
-    echo $result
+    $result = & (Resolve-Path $esPath).Path $SearchTerm | ? {$_ -notmatch '.manifest'} | ? {$_ -notmatch '.dll'}| ? {$_ -notmatch '.service'} | ? {$_ -notmatch '.timer'} | ? {$_ -notmatch '.dsh'} | ? {$_ -notmatch '.cpp'} | ? {$_ -notmatch '.py'}
+     echo $result
 }
 
 Function Open-ESSearchResult {
